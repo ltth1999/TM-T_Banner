950d0fca00f9b9d3f7ae2db1d6338c524498b48e
sinhvien <sinhvien>
2020-10-23 10:25:17 +0700

    Banner

---
+++ Buoi3/Buoi3/Controllers/BannerController.cs
@@ -0,0 +1,34 @@
+﻿using Buoi3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Buoi3.Controllers
+{
+    public class BannerController : Controller
+    {
+        TMĐTEntities db = new TMĐTEntities();
+        // GET: Banner
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HeaderBanner()
+        {
+            var headerBanner = db.Banners.Where(c => c.Name == "Header").FirstOrDefault();
+            return PartialView(headerBanner);
+        }
+        public ActionResult LeftBanner()
+        {
+            var leftBanner = db.Banners.Where(c => c.Name == "Left").FirstOrDefault();
+            return PartialView(leftBanner);
+        }
+        public ActionResult RightBanner()
+        {
+            var rightBanner = db.Banners.Where(c => c.Name == "Right").FirstOrDefault();
+            return PartialView(rightBanner);
+        }
+    }
+}
---
+++ Buoi3/Buoi3/Controllers/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using Buoi3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Buoi3.Controllers
+{
+    public class MenuController : Controller
+    {
+        TMĐTEntities db = new TMĐTEntities();
+        // GET: Menu
+        public ActionResult Index()
+        {
+            var menus = db.Menus;
+            return PartialView(menus);
+        }
+    }
+}
